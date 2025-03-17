a9ce44df93ee9392765b4799bc50296583e9b652
Talion <167514459+AlluinEdouard@users.noreply.github.com>
2025-03-17 15:55:51 +0100

    new branch

--- src/user/user.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { createUser, fetchUser, login } from './user.controller'
-
-export const userRouter = Router()
-
-userRouter.get('/:userId', fetchUser)
-userRouter.post('/', createUser)
-userRouter.post('/login', login)
